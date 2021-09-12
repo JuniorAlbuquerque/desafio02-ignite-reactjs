b6e1b2a45d56d21b525b2de01d464a8ad9765ab5
Ednilson Albuquerque <ednilson.albuquerque@conexasaude.com.br>
2021-09-12 19:03:03 -0400

    refactor: sidebar and content

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,38 @@
-export function SideBar() {
-  // Complete aqui
+import { GenreResponseProps } from "../App";
+import { Button } from "./Button";
+import '../styles/sidebar.scss';
+import { useEffect, useState } from "react";
+import { api } from "../services/api";
+
+interface SidebarProps {
+  onChange: (id: number) => void
+  selectedGenreId: number
+}
+
+export function SideBar({ onChange, selectedGenreId }: SidebarProps) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onChange(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
 }
